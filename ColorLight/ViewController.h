80be6cb901c3ee53da01cfb3fdc0a47882c2c806
mits <mits.jp@gmail.com>
2013-12-03 23:10:33 +0900

    setup GitHub

--- ColorLight/ViewController.h
+++ ColorLight/ViewController.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+- (IBAction)switchOnOff:(UISwitch *)sender;
 
 @end
--- ColorLight/ViewController.m
+++ ColorLight/ViewController.m
@@ -26,4 +26,11 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)switchOnOff:(UISwitch *)sender {
+    if (sender.on) {
+        self.view.backgroundColor = [UIColor greenColor];
+    } else {
+        self.view.backgroundColor = [UIColor whiteColor];
+    }
+}
 @end
