b4a2d47a64fd68cbe23e35f15b95d4a7b1231ffa
mits <mits.jp@gmail.com>
2013-12-07 22:59:54 +0900

    ターゲットデバイスを変更

--- ColorLight/ViewController.m
+++ ColorLight/ViewController.m
@@ -26,11 +26,13 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)switchOnOff:(UISwitch *)sender {
+- (IBAction)switchOnOff:(UISwitch *)sender
+{
     if (sender.on) {
         self.view.backgroundColor = [UIColor greenColor];
     } else {
         self.view.backgroundColor = [UIColor whiteColor];
     }
 }
+
 @end
